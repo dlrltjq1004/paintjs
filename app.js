56373f5e5621ba184db2e715551a3faf9de32bd6
dlrltjq1004 <48882958+dlrltjq1004@users.noreply.github.com>
2019-09-17 00:58:51 +0900

    ChangeColorClick

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-
+const colors = document.getElementsByClassName("jsColor");
 canvas.width = 700;
 canvas.height = 700;
 
@@ -37,6 +37,10 @@ function onMouseUp(event) {
     stopPainting();
 }
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -44,3 +48,5 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
